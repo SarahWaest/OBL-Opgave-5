f3c21eccef590d5b4bb6b202b6498f34745e71b9
Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>
2020-03-10 11:12:20 +0100

    Opgave 5

---
+++ Opgave5/Bog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Opgave5
+{
+    public class Bog
+    {
+        private int _sidetal;
+        private string _forfatter;
+        private string _titel;
+        private string _isbn13;
+
+        public Bog()
+        {
+
+        }
+
+        public Bog(int sidetal, string forfatter, string titel, string isbn13)
+        {
+            _sidetal = sidetal;
+            _forfatter = forfatter;
+            _titel = titel;
+            _isbn13 = isbn13;
+        }
+
+        public string Titel
+        {
+            get => _titel;
+            set => _titel = value;
+        }
+
+        public string Forfatter
+        {
+            get => _forfatter;
+            set
+            {
+                if (value.Length < 2) throw new ArgumentOutOfRangeException();
+                _forfatter = value;
+            }
+        }
+
+        public int Sidetal
+        {
+            get => _sidetal;
+            set
+            {
+                if (value <= 4 || value >= 1000) throw new ArgumentOutOfRangeException();
+                _sidetal = value;
+            }
+        }
+
+        public string Isbn13
+        {
+            get => _isbn13;
+            set
+            {
+                if (value.Length > 13 || value.Length < 11) throw new ArgumentOutOfRangeException();
+                _isbn13 = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Forfatter + " " + Titel + " " + Sidetal + " " + Isbn13;
+        }
+    }
+}
---
+++ Opgave5/Server.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Opgave5
+{
+
+    public class Server
+    {
+        private static List<Bog> _bogliste = new List<Bog>()
+        {
+            new Bog() {Titel = "Programmering", Forfatter = "Claus Hedegaard", Isbn13 = "123456789123", Sidetal = 84},
+            new Bog()
+            {
+                Titel = "Programmering 1.3", Forfatter = "Claus Hedegaard", Isbn13 = "123456789124", Sidetal = 100
+            },
+            new Bog()
+            {
+                Titel = "Programmering 1.1", Forfatter = "Claus Hedegaard", Isbn13 = "123456789125", Sidetal = 400
+            },
+            new Bog()
+            {
+                Titel = "Programmering 1.2", Forfatter = "Claus Hedegaard", Isbn13 = "123456789126", Sidetal = 900
+            },
+            new Bog()
+            {
+                Titel = "Programmering 1.3", Forfatter = "Claus Hedegaard", Isbn13 = "123456789127", Sidetal = 700
+            }
+        };
+
+        public Server()
+        {
+            
+        }
+
+        public void DoClient(TcpClient socket)
+        {
+            using (socket)
+            {
+                NetworkStream ns = socket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+
+                string line;
+                while (true)
+                {
+                    line = sr.ReadLine();
+
+                    Console.WriteLine("Client:" + line);
+
+                    string[] stringSplit = line.Split(' ');
+
+                    if (stringSplit[0] == "GetAll")
+                    {
+                        string bog = JsonConvert.SerializeObject(_bogliste);
+
+                        Console.WriteLine(socket.Client.RemoteEndPoint + ": " + bog);
+
+                        sw.WriteLine(bog);
+                    }
+
+                    if (stringSplit[0] == "GetByISBN13")
+                    {
+                        if (stringSplit.Length == 2)
+                        {
+                            Bog bogen = _bogliste.Find(bog => bog.Isbn13 == stringSplit[1]);
+                            sw.WriteLine(JsonConvert.SerializeObject(bogen));
+                        }
+                    }
+
+                    if (stringSplit[0] == "Save")
+                    {
+                        if (stringSplit.Length >= 2)
+                        {
+                            Bog nyBog = JsonConvert.DeserializeObject<Bog>(stringSplit[1]);
+                            sw.WriteLine("Tilføjer ny bog ved navn:" + nyBog.Titel);
+                            _bogliste.Add(nyBog);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 4646);
+            listener.Start();
+
+            Console.WriteLine("Server startet");
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Console.WriteLine("Server er forbundet til en client");
+
+                Task.Run(() =>
+                {
+                    TcpClient tempsocket = socket;
+                    DoClient(tempsocket);
+                });
+            }
+        }
+
+    }
+}
